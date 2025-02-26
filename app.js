389ffdba1aa5887e1ed074748f7d6223389b99c9
minhnguyen9702 <minhnguyen9702@gmail.com>
2025-02-26 15:33:50 +0700

    add layout.ejs which all ejs views will use

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("node:path");
 const express = require("express");
 const session = require("express-session");
+const expressLayouts = require("express-ejs-layouts");
 const passport = require("./auth");
 const { PrismaClient } = require("@prisma/client");
 const PrismaSessionStore =
@@ -9,6 +10,11 @@ const PrismaSessionStore =
 const prisma = new PrismaClient();
 
 const app = express();
+// use ejs for views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layout");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,11 +35,33 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// use ejs for views
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// authentication middleware + making sure user is global
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+};
+app.use((req, res, next) => {
+  if (req.user) {
+    res.locals.user = {
+      id: req.user.id,
+      username: req.user.username,
+    };
+  } else {
+    res.locals.user = null;
+  }
+  res.locals.errorMessage = req.flash("error");
+  next();
+});
 
+//importing routers
 const userRouter = require("./routes/userRouter");
+
+//setting up routes
+app.get("/", ensureAuthenticated, (req, res) => {
+  res.render("index", { user: req.user });
+});
 app.use("/user", userRouter);
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
